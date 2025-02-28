eded4ace3a4fc54099781ce5ea26f16d3fcf1959
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-02-28 08:45:56 -0600

    Update company routes

--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -16,11 +16,12 @@ import {
     isAdmin,
     validateJwt
 } from '../../middlewares/validate.jwt.js'
+import { companyValidator } from '../../middlewares/validators.js'
 
 
 const api = Router()
 
-api.post('/addCompany', validateJwt, isAdmin, addCompany)
+api.post('/addCompany', validateJwt, isAdmin , companyValidator, addCompany)
 api.get('/listCompany', validateJwt, isAdmin, listCompany)
 api.put('/updateCompany/:id', validateJwt, isAdmin, updateCompany)
 
