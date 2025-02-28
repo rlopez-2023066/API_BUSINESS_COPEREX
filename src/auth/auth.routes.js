e12409c3d0e7cc01de31ec1d015d16810f117a4b
Alfredo López <rlopez-2023066@kinal.edu.gt>
2025-02-28 08:46:09 -0600

    Update Auth Router

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -4,10 +4,11 @@ import {
     login,
     register
 } from '../auth/auth.controller.js'
+import { registerValidator } from '../../middlewares/validators.js'
 
 const api = Router()
 
-api.post('/register', register)
+api.post('/register', registerValidator, register)
 api.post('/login', login)
 
 export default api
